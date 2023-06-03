6429bb34a03af5fb06080bb44f2b206613055679
MarlonBair <98485639+MarlonBair@users.noreply.github.com>
2023-06-03 00:28:46 -0700

    6/2/23

---
+++ TreeLevelOrderTraversal.java
@@ -0,0 +1,61 @@
+/**
+ * @author Marlon Bair 6/2/23
+ * 
+ * Given the root of a binary tree, return the bottom-up level order traversal of its nodes' values. 
+ * (i.e., from left to right, level by level from leaf to root).
+ */
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TreeLevelOrderTraversal {
+    
+  // Definition for a binary tree node.
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        
+        // ArrayList is most efficient as new level will be added at tail
+        List<List<Integer>> list = new ArrayList<>();
+
+        if(root == null) { return list; }
+
+        // Utilize queue for a BFS at each sublevel of tree
+        Queue<TreeNode> q = new LinkedList<>();
+
+        q.add(root);
+
+        // Using a BFS, add each sublevel of tree to total list
+        while(!q.isEmpty()) {
+            List<Integer> currentLevel = new ArrayList<Integer>();
+            
+            int levelSize = q.size();                               
+            for(int i = 0; i < levelSize; i++) {
+
+                if(q.peek().left != null) { q.add(q.peek().left); }
+
+                if(q.peek().right != null) { q.add(q.peek().right); }
+
+                currentLevel.add(q.remove().val);
+            }
+            list.add(currentLevel);
+
+        }
+
+        return list;
+        
+    }
+}
+
